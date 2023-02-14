9e0238dc7d0906f3df52cdc183f40eebd9890014
brunofelixf <brunofelixf@gmail.com>
2023-02-13 22:00:11 -0300

    refactor: adjust tests

--- tests/user/user.spec.ts
+++ tests/user/user.spec.ts
@@ -1,6 +1,7 @@
-import { prisma } from './../../src/server';
+import { prisma } from '../../src/server';
 import request  from "supertest";
 import { server } from "../../src/server";
+import { Response } from 'superagent';
 
 describe('Criar Usuário | Route', () => {
 
@@ -43,6 +44,19 @@ describe('Criar Usuário | Route', () => {
 
 describe('Listar dados do usuário | Route', () => {
 
+    const data = {
+        email: "test@email.com",
+        password: "123"
+    }
+
+    let login: Response
+
+    beforeAll( async () => { 
+        login = await request(server)
+        .post('/login')
+        .send( data ) 
+    })
+
     it('Deve retornar erro se não estiver logado e retornar status 401', async () => {
         const response = await request(server)
         .get('/user')
@@ -51,17 +65,10 @@ describe('Listar dados do usuário | Route', () => {
     })
 
     it('Deve retornar dados da conta logada e retornar status 200', async () => {
-        const data = {
-            email: "test@email.com",
-            password: "123"
-        }
-        const response = await request(server)
-        .post('/login')
-        .send( data )
 
         const user = await request(server)
         .get('/user')
-        .set({ Authorization: `Bearer ${response.body.token}`})
+        .set({ Authorization: `Bearer ${login.body.token}`})
         .expect(200)
         expect( user.body[0] ).toHaveProperty( 'user_id') 
     })
@@ -69,6 +76,19 @@ describe('Listar dados do usuário | Route', () => {
 
 describe('Atualizar dados do usuário | Route', () => {
 
+    const data = {
+        email: "test@email.com",
+        password: "123"
+    }
+
+    let login: Response
+
+    beforeAll( async () => { 
+        login = await request(server)
+        .post('/login')
+        .send( data ) 
+    })
+    
     afterAll( async () => {
         await prisma.user.delete({ where: { email: 'test@email.com' }})
     })
@@ -81,19 +101,18 @@ describe('Atualizar dados do usuário | Route', () => {
     })
 
     it('Deve atualizar dados da conta logada e retornar status 200', async () => {
-        const data = {
-            email: "test@email.com",
-            password: "123"
-        }
-        const response = await request(server)
-        .post('/login')
-        .send( data )
-
         const user = await request(server)
         .patch('/user')
         .send( { name: "Usuário Teste Atualizado" } )
-        .set({ Authorization: `Bearer ${response.body.token}`})
+        .set({ Authorization: `Bearer ${login.body.token}`})
         .expect(200)
         expect( user.body.name ).toBe( "Usuário Teste Atualizado" ) 
+    })
+
+    it('Deve deletar dados da conta logada e retornar status 204', async () => {
+        await request(server)
+        .delete('/user')
+        .set({ Authorization: `Bearer ${login.body.token}`})
+        .expect(204)
     })
 });
