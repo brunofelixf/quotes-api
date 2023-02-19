b7ff6174329ec377db8da3cf0a7d5e2b6343ff00
brunofelixf <brunofelixf@gmail.com>
2023-02-19 02:03:21 -0300

    build: add new files and configs

--- dist/src/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const databaseUrl_1 = require("./utils/databaseUrl");
-console.log('Environment: ' + process.env.NODE_ENV);
-(0, databaseUrl_1.setDatabaseUrl)();
-const port = 3000;
-app_1.app.listen(port, () => console.log('Server running (Port): ' + port));
-//# sourceMappingURL=server.js.map
